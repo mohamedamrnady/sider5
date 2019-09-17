6c727bd54ef7db393a7dc7042a8627a2ca3646a4
Anton Jouline <juce66@gmail.com>
2019-09-17 12:05:49 -0700

    support for trophy_check event; tweak for stadium_choice behaviour

--- sider.cpp
+++ sider.cpp
@@ -200,7 +200,7 @@ struct TROPHY_TABLE_ENTRY {
     DWORD trophy_id;
 };
 
-#define TT_LEN 0x14b
+#define TT_LEN 0x148
 TROPHY_TABLE_ENTRY _trophy_table[TT_LEN];
 typedef unordered_map<WORD,DWORD> trophy_map_t;
 trophy_map_t *_trophy_map;
@@ -4499,10 +4499,10 @@ void sider_free_select(BYTE *controller_restriction)
 void sider_trophy_table(TROPHY_TABLE_ENTRY *tt)
 {
     //logu_("trophy table addr: %p\n", tt);
-    //logu_("tid: %d (0x%x) --> 0x%x\n", tt->tournament_id, tt->trophy_id);
     EnterCriticalSection(&_tcs);
     for (int i=0; i<TT_LEN; i++) {
         (*_trophy_map)[tt->tournament_id] = tt->trophy_id;
+        //logu_("tid: %d (0x%x) --> 0x%x\n", tt->tournament_id, tt->tournament_id, tt->trophy_id);
         tt++;
     }
     LeaveCriticalSection(&_tcs);
@@ -4551,8 +4551,10 @@ STAD_INFO_STRUCT* sider_def_stadium_name(DWORD stadium_id)
 void sider_set_stadium_choice(MATCH_INFO_STRUCT *mi, BYTE stadium_choice)
 {
     _stadium_choice_count++;
+    DBG(16) logu_("set_stadium_choice: mi->stadium_choice=%d, stadium_choice=%d\n", mi->stadium_choice, stadium_choice);
+    bool just_updated = (mi->stadium_choice != stadium_choice);
     mi->stadium_choice = stadium_choice;
-    if (_stadium_choice_count % 2 == 1) {
+    if (!just_updated) {//_stadium_choice_count % 2 == 1) {
         if (_config->_lua_enabled) {
             // lua callbacks
             vector<module_t*>::iterator i;
@@ -6303,10 +6305,10 @@ bool all_found(config_t *cfg) {
     }
     if (cfg->_lua_enabled) {
         all = all && (
-            //cfg->_hp_at_set_team_id > 0 &&
-            //cfg->_hp_at_set_settings > 0 &&
-            //cfg->_hp_at_trophy_check > 0 &&
-            //cfg->_hp_at_trophy_table > 0 &&
+            cfg->_hp_at_set_team_id > 0 &&
+            cfg->_hp_at_set_settings > 0 &&
+            cfg->_hp_at_trophy_check > 0 &&
+            cfg->_hp_at_trophy_table > 0 &&
             cfg->_hp_at_ball_name > 0 &&
             //cfg->_hp_at_stadium_name > 0 &&
             //cfg->_hp_at_def_stadium_name > 0 &&
